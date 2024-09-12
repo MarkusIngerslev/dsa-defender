8be2ce2c54f427b40b812b5ec31f0d2ec2f86673
MarkusIngerslev <markusolsen2004@gmail.com>
2024-09-12 09:54:37 +0200

    Tilføjet manglende methode områder samt tilføjet beskrivende kommentar til hver af dem

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -1,6 +1,7 @@
 export default class SinglyLinkedList {
     head = null;
 
+    // Der opretter en ny node, med link til data-objektet, og tilføjer den til listen
     add(enemy) {
         const node = new Node(enemy);
         if (this.head == null) {
@@ -12,19 +13,50 @@ export default class SinglyLinkedList {
         }
     }
 
-    getFirstNode() {
-        return this.head;
+    // Der finder en node med link til dét data-objekt, og fjerner den noden.
+    remove(data) {
+        const nodeToRemove = this.getNodeWith(data);
+        if (nodeToRemove) {
+            console.log(nodeToRemove);
+            this.removeNode(nodeToRemove);
+        }
     }
 
-    getNextNode(node) {
-        return node.next;
-    }
-
+    // Der returnerer data i den første node i listen
     getFirst() {
         const node = this.getFirstNode();
         return node.data;
     }
 
+    // Der returnerer data-objektet på det pågældende index i listen.
+    getLast() {
+        let current = this.head;
+        while (current.next !== null) {
+            current = current.next;
+        }
+        return current.data;
+    }
+
+    // der returnerer den første node i listen
+    getFirstNode() {
+        return this.head;
+    }
+
+    // Der returnerer noden efter denne (eller null, hvis der ikke er nogen)
+    getNextNode(node) {
+        return node.next;
+    }
+
+    // Der returnerer den sidste node i listen
+    getLastNode() {
+        let current = this.head;
+        while (current.next !== null) {
+            current = current.next;
+        }
+        return;
+    }
+
+    // Der returnerer den node der linker til dette data-objekt
     getNodeWith(data) {
         let current = this.head;
         while (current !== null) {
@@ -36,7 +68,15 @@ export default class SinglyLinkedList {
         }
     }
 
-    // Fjerner en node fra listen
+    // der fjerner den første node fra listen
+    removeFirstNode() {
+        
+    }
+
+    // Der fjerner den sidste node fra listen
+    removeLastNode() {}
+
+    // Der fjerner dén node fra listen
     removeNode(node) {
         // Hvis noden der skal fjernes er head
         if (this.head === node) {
@@ -56,16 +96,14 @@ export default class SinglyLinkedList {
         }
     }
 
-    // Fjerner node med bestemt data
-    remove(data) {
-        const nodeToRemove = this.getNodeWith(data);
-        if (nodeToRemove) {
-            console.log(nodeToRemove);
-            this.removeNode(nodeToRemove);
-        }
-    }
+    // der fjerner alle nodes fra listen, og sørger for at den er tom
+    clear() {}
 
+    // der returnerer antallet af nodes i listen
     size() {}
+
+    // der console.log'er alle data-elementer i listen
+    dumpList() {}
 }
 
 class Node {
